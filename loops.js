6bd267b2ad3941dbf40de400cfd99f4de75219f0
yvonneeeee <yvonnelin840701@gmail.com>
2021-03-09 09:13:03 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,26 @@
+function forLoop(array){
+  for ( let i = 0  ; i < 25 ; i+=1){
+    if (i === 1){
+      array.push(`I am ${i} strange loop.`)
+    }else {
+      array.push(`I am ${i} strange loops.`)
+    }
+  }
+  return array
+}
+
+
+function whileLoop(n){
+    let i = n;
+    while (i >0){
+      console.log(i--);
+    }
+    return 'done'
+}
+
+function doWhileLoop(num){
+  do{
+  console.log("I run once regardless.")
+  }while (num == 0);
+
+}
